2beaf084592892b6078f1576f6f3f79169f96235
a3brx <a3brx@sapo.pt>
2020-10-16 10:59:43 +0100

    isset verification for get method

--- php/sum.php
+++ php/sum.php
@@ -7,11 +7,13 @@
 </head>
 <body>
 <?php
-$num1 = $_GET['num1'];
-$num2 = $_GET['num2'];
-$sum = $num1 + $num2;
+if (isset($_GET['num1']) && isset($_GET['num2'])) {
+    $num1 = $_GET['num1'];
+    $num2 = $_GET['num2'];
+    $sum = $num1 + $num2;
 
-echo "<p>$sum</p>";
+    echo "<p>$sum</p>";
+}
 ?>
 </body>
 </html>
