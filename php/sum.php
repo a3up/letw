230d8de9fd3926301c943dd3f210c91cb976c5a0
a3brx <a3brx@sapo.pt>
2020-10-16 10:56:05 +0100

    sum

---
+++ php/sum.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$num1 = $_GET['num1'];
+$num2 = $_GET['num2'];
+$sum = $num1 + $num2;
+
+echo "<p>$sum</p>";
+?>
+</body>
+</html>
